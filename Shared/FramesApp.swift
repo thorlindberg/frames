c6a003edf56beb4161c10915fff7714dad58d238
Thor <thorlindberg@icloud.com>
2021-05-06 18:11:49 +0200

    Initial Commit

---
+++ Shared/FramesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramesApp.swift
+//  Shared
+//
+//  Created by Thor Lindberg on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FramesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
