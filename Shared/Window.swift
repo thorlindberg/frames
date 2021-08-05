c3e1179d9a1c35ee10ffd1dc9fc052360eed28b1
Thor Lindberg <71700400+thorskjold@users.noreply.github.com>
2021-08-05 19:12:38 +0200

    Landmark

--- Shared/Window.swift
+++ Shared/Window.swift
@@ -1,166 +1,222 @@
 import SwiftUI
 import UIKit
 import AVFoundation
-import AVFoundation
 
 struct Window: View {
     
     @ObservedObject var model: Model
 
     var body: some View {
-        ZStack {
-            if model.data.isAugmenting {
-                ARViewContainer(model: model)
-                    .ignoresSafeArea()
-            } else {
-                CameraView()
-                    .ignoresSafeArea()
-            }
-            if model.data.isBlurred {
-                Blur(style: .regular)
-                    .ignoresSafeArea()
-            }
-            VStack {
-                Spacer()
-                if !model.data.isAugmenting {
-                    if model.data.isFramed {
-                        Image(uiImage: model.data.frame!)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding(50)
-                    } else {
-                        Text("[ Info here ]")
-                    }
+        GeometryReader { device in
+            ZStack {
+                if model.data.isAugmenting {
+                    ARViewContainer(model: model)
+                        .ignoresSafeArea()
+                } else {
+                    CameraView()
+                        .ignoresSafeArea()
                 }
-                Spacer()
-                HStack {
+                if model.data.isBlurred {
+                    Blur(style: .regular)
+                        .ignoresSafeArea()
+                }
+                VStack {
                     Spacer()
-                    if model.data.isAugmenting {
-                        Button(action: {
-                            model.data.isFlashlight.toggle()
-                            toggleTorch(on: model.data.isFlashlight)
-                        }) {
+                    if !model.data.isAugmenting {
+                        if model.data.isFramed {
+                            Image(uiImage: model.data.frame!)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(50)
+                            Spacer()
+                        } else {
                             ZStack {
-                                Blur(style: .dark)
-                                    .mask(Circle())
-                                Image(systemName: model.data.isFlashlight ? "bolt.fill" : "bolt.slash")
-                                    .font(.system(size: 20))
+                                HStack(alignment: .bottom) {
+                                    Spacer()
+                                    Rectangle()
+                                        .frame(width: 3, height: (device.size.height - 130 - 100) / 6 * 6)
+                                    Spacer()
+                                    Rectangle()
+                                        .frame(width: 3, height: (device.size.height - 130 - 100) / 6 * 1)
+                                        .padding(.horizontal, 67 / 2)
+                                    Spacer()
+                                    Rectangle()
+                                        .frame(width: 3, height: (device.size.height - 130 - 100) / 6 * 4)
+                                    Spacer()
+                                }
+                                .opacity(0.25)
+                                VStack {
+                                    Text("Add an image")
+                                    Text("to create a frame")
+                                    Spacer()
+                                    Text("Define dimensions")
+                                    Text("manually or with AR")
+                                    Spacer()
+                                    Text("View frame in AR")
+                                    Text("when photo added")
+                                        .padding(.bottom, (device.size.height - 130 - 100) / 6 * 1 + 30)
+                                }
+                                .opacity(0.75)
+                                .frame(height: (device.size.height - 130 - 100) / 6 * 6)
                             }
-                            .frame(width: 50, height: 50)
-                        }
-                    } else {
-                        Menu {
-                            Button(action: {
-                                model.data.isImporting.toggle()
-                            }) {
-                                Label("Choose Photo", systemImage: "photo")
-                            }
-                            Button(action: {
-                                model.data.isCapturing.toggle()
-                            }) {
-                                Label("Capture Photo", systemImage: "camera")
-                            }
-                            Button(action: {
-                                UIApplication.shared.windows.filter({$0.isKeyWindow})
-                                    .first?.rootViewController?
-                                    .present(model.getDocumentCameraViewController(), animated: true, completion: nil)
-                            }) {
-                                Label("Scan Photo", systemImage: "viewfinder")
-                            }
-                        } label: {
-                            ZStack {
-                                Blur(style: .dark)
-                                    .mask(Circle())
-                                Image(systemName: "camera.fill")
-                            }
-                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
                         }
                     }
-                    Spacer()
-                    Button(action: {
+                    HStack {
+                        Spacer()
                         if model.data.isAugmenting {
-                            withAnimation {
-                                model.data.isBlurred.toggle()
-                            }
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                withAnimation {
-                                    model.data.isAugmenting.toggle()
+                            Button(action: {
+                                model.data.isFlashlight.toggle()
+                                toggleTorch(on: model.data.isFlashlight)
+                            }) {
+                                ZStack {
+                                    ZStack {
+                                        Blur(style: .dark)
+                                            .mask(Circle())
+                                        Image(systemName: model.data.isFlashlight ? "bolt.fill" : "bolt.slash")
+                                            .font(.system(size: 20))
+                                    }
+                                    .frame(width: 50, height: 50)
+                                    Rectangle()
+                                        .opacity(0)
+                                        .frame(width: 100, height: 100)
                                 }
                             }
                         } else {
-                            model.writeScene() // writes frame to app documents
-                            withAnimation {
-                                model.data.isAugmenting.toggle()
-                            }
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                withAnimation {
-                                    model.data.isBlurred.toggle()
+                            Menu {
+                                Button(action: {
+                                    model.data.isImporting.toggle()
+                                }) {
+                                    Label("Choose Photo", systemImage: "photo")
+                                }
+                                Button(action: {
+                                    model.data.isCapturing.toggle()
+                                }) {
+                                    Label("Capture Photo", systemImage: "camera")
+                                }
+                                Button(action: {
+                                    UIApplication.shared.windows.filter({$0.isKeyWindow})
+                                        .first?.rootViewController?
+                                        .present(model.getDocumentCameraViewController(), animated: true, completion: nil)
+                                }) {
+                                    Label("Scan Photo", systemImage: "viewfinder")
+                                }
+                            } label: {
+                                ZStack {
+                                    ZStack {
+                                        Blur(style: .dark)
+                                            .mask(Circle())
+                                        Image(systemName: "camera.fill")
+                                    }
+                                    .frame(width: 50, height: 50)
+                                    Rectangle()
+                                        .opacity(0)
+                                        .frame(width: 100, height: 100)
                                 }
                             }
                         }
-                    }) {
-                        ZStack {
-                            Circle()
-                                .foregroundColor(.white)
-                            Image(systemName: "arrow.up")
-                                .font(.system(size: 30))
-                                .rotation3DEffect(
-                                    .degrees(model.data.isAugmenting ? 180 : 0),
-                                    axis: (x: 1.0, y: 0.0, z: 0.0)
-                                )
-                        }
-                        .frame(width: 70, height: 70)
-                    }
-                    .disabled(!model.data.isFramed)
-                    Spacer()
-                    if model.data.isAugmenting {
-                        Menu {
-                            Button(action: {
-                                model.data.alignment = "none"
-                            }) {
-                                Label("No alignment", systemImage: model.data.alignment == "none" ? "checkmark" : "arrow.up.left.and.arrow.down.right")
-                            }
-                            .disabled(model.data.alignment == "none")
-                            Button(action: {
-                                model.data.alignment = "vertical"
-                            }) {
-                                Label("Vertical alignment", systemImage: model.data.alignment == "vertical" ? "checkmark" : "rectangle.arrowtriangle.2.inward")
-                            }
-                            .disabled(model.data.alignment == "vertical")
-                            Button(action: {
-                                model.data.alignment = "horizontal"
-                            }) {
-                                Label("Horizontal alignment", systemImage: model.data.alignment == "horizontal" ? "checkmark" : "rectangle.portrait.arrowtriangle.2.inward")
-                            }
-                            .disabled(model.data.alignment == "horizontal")
-                        } label: {
-                            ZStack {
-                                Blur(style: .dark)
-                                    .mask(Circle())
-                                Image(systemName: "perspective")
-                                    .font(.system(size: 20))
-                            }
-                            .accentColor(.orange)
-                            .frame(width: 50, height: 50)
-                        }
-                    } else {
+                        Spacer()
                         Button(action: {
-                            //
+                            if model.data.isAugmenting {
+                                withAnimation {
+                                    model.data.isBlurred.toggle()
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    withAnimation {
+                                        model.data.isAugmenting.toggle()
+                                    }
+                                }
+                            } else {
+                                model.writeScene() // writes frame to app documents
+                                withAnimation {
+                                    model.data.isAugmenting.toggle()
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    withAnimation {
+                                        model.data.isBlurred.toggle()
+                                    }
+                                }
+                            }
                         }) {
                             ZStack {
-                                Blur(style: .dark)
-                                    .mask(Circle())
-                                Image(systemName: "cube")
-                                    .font(.system(size: 20))
+                                if model.data.isFramed {
+                                    Circle()
+                                        .foregroundColor(.white)
+                                } else {
+                                    Blur(style: .light)
+                                        .mask(Circle())
+                                }
+                                Image(systemName: "arrow.up")
+                                    .font(.system(size: 30))
+                                    .foregroundColor(!model.data.isFramed ? .black : nil)
+                                    .rotation3DEffect(
+                                        .degrees(model.data.isAugmenting ? 180 : 0),
+                                        axis: (x: 1.0, y: 0.0, z: 0.0)
+                                    )
                             }
-                            .accentColor(.orange)
-                            .frame(width: 50, height: 50)
+                            .frame(width: 70, height: 70)
                         }
+                        .disabled(!model.data.isFramed)
+                        Spacer()
+                        if model.data.isAugmenting {
+                            Menu {
+                                Button(action: {
+                                    model.data.alignment = "none"
+                                }) {
+                                    Label("No alignment", systemImage: model.data.alignment == "none" ? "checkmark" : "arrow.up.left.and.arrow.down.right")
+                                }
+                                .disabled(model.data.alignment == "none")
+                                Button(action: {
+                                    model.data.alignment = "vertical"
+                                }) {
+                                    Label("Vertical alignment", systemImage: model.data.alignment == "vertical" ? "checkmark" : "rectangle.arrowtriangle.2.inward")
+                                }
+                                .disabled(model.data.alignment == "vertical")
+                                Button(action: {
+                                    model.data.alignment = "horizontal"
+                                }) {
+                                    Label("Horizontal alignment", systemImage: model.data.alignment == "horizontal" ? "checkmark" : "rectangle.portrait.arrowtriangle.2.inward")
+                                }
+                                .disabled(model.data.alignment == "horizontal")
+                            } label: {
+                                ZStack {
+                                    ZStack {
+                                        Blur(style: .dark)
+                                            .mask(Circle())
+                                        Image(systemName: "perspective")
+                                            .font(.system(size: 20))
+                                    }
+                                    .accentColor(.orange)
+                                    .frame(width: 50, height: 50)
+                                    Rectangle()
+                                        .opacity(0)
+                                        .frame(width: 100, height: 100)
+                                }
+                            }
+                        } else {
+                            Button(action: {
+                                //
+                            }) {
+                                ZStack {
+                                    ZStack {
+                                        Blur(style: .dark)
+                                            .mask(Circle())
+                                        Image(systemName: "cube")
+                                            .font(.system(size: 20))
+                                    }
+                                    .accentColor(.orange)
+                                    .frame(width: 50, height: 50)
+                                    Rectangle()
+                                        .opacity(0)
+                                        .frame(width: 100, height: 100)
+                                }
+                            }
+                        }
+                        Spacer()
                     }
-                    Spacer()
                 }
+                .padding(.bottom, 15)
             }
-            .padding(.bottom, 30)
         }
         .fullScreenCover(isPresented: $model.data.isImporting) {
             ImagePicker(model: model, type: "import")
@@ -177,6 +233,7 @@ struct Window: View {
 struct Window_Previews: PreviewProvider {
     static var previews: some View {
         Window(model: Model())
+            .previewDevice("iPhone 12 mini")
     }
 }
 
